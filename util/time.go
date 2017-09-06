791735d2e67a7f43e5ff8027e183360bdb71b5f2
Deepak Kumar <kumarde@umich.edu>
2017-09-06 12:13:34 -0500

    run main faster

--- util/time.go
+++ util/time.go
@@ -21,7 +21,6 @@ var (
 	CABReservedIPDate          = time.Date(2016, time.October, 1, 0, 0, 0, 0, time.UTC)
 	CABGivenNameDate           = time.Date(2016, time.September, 7, 0, 0, 0, 0, time.UTC)
 	CABSerialNumberEntropyDate = time.Date(2016, time.September, 30, 0, 0, 0, 0, time.UTC)
-	CABGivenNameDate           = time.Date(2016, time.September, 7, 0, 0, 0, 0, time.UTC)
 	CABV102Date                = time.Date(2012, time.June, 8, 0, 0, 0, 0, time.UTC)
 	CABV113Date                = time.Date(2013, time.February, 21, 0, 0, 0, 0, time.UTC)
 	CABV114Date                = time.Date(2013, time.May, 3, 0, 0, 0, 0, time.UTC)
